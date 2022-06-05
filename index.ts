bbafd2ba66b97a5af75c03f0f06ea18da8241235
nkruger <nicolas.nkruger@gmail.com>
2022-06-05 10:36:13 -0300

    feture [set (open | close), get (state)]

---
+++ index.ts
@@ -0,0 +1,35 @@
+import express from 'express';
+
+const port = 3333;
+
+
+const doorState = ['open', 'close'] as const;
+type DoorState = typeof doorState[number];
+
+
+
+let door:DoorState = "open";
+
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json({
+        door
+    })
+})
+
+app.post('/:state', (req, res) => {
+    
+    const state = req.params.state as DoorState;
+
+    door = doorState.includes(state) ? state : door;
+
+    res.json({
+        door
+    })
+})
+
+app.listen(port, () => {
+    console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+})
